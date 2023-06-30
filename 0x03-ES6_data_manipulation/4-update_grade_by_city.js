37ebda65de5eb2a57ffc53224eb7b8ba1a71075b
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2023-06-30 19:23:59 +0000

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,11 @@
 export default function updateStudentGradeByCity(studentList, city, newGrades) {
-  return studentList
-    .filter(student => student.city === city)
+  const updatedStudents = studentList
+    .filter(student => student.location === city)
     .map(student => {
       const gradeObj = newGrades.find(grade => grade.studentId === student.id);
       const grade = gradeObj ? gradeObj.grade : 'N/A';
       return { ...student, grade };
     });
+
+  console.log(JSON.stringify(updatedStudents, null, 2));
 }
