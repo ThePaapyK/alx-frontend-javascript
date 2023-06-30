d3a565376590ac98567bfa80c9c95ade9b966f64
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2023-06-30 18:49:06 +0000

    Create 8-clean_set.js

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,5 @@
+function cleanSet(set, startString) {
+  const filteredValues = [...set].filter(value => value.startsWith(startString));
+  const cleanedString = filteredValues.join('-');
+  return cleanedString;
+}
