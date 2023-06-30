299b59dd7009c5afd175d1faf13a6606ce03e61f
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2023-06-30 19:12:43 +0000

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,9 @@
 export default function cleanSet(set, startString) {
+  if (!(set instanceof Set)) {
+    throw new Error('Invalid argument: set must be a Set');
+  }
+
   const filteredValues = [...set].filter(value => value.startsWith(startString));
-  const cleanedString = filteredValues.join('-');
+  const cleanedString = filteredValues.map(value => value.slice(startString.length)).join('-');
   return cleanedString;
 }
