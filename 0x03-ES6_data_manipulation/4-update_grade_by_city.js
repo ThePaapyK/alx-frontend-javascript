466ed07d81ef2cca19abc419f6e9b1d29cc9ff1e
Paa Kojo Annan <pkeannan77@gmail.com>
2023-07-01 01:57:56 +0000

    Mensuro

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,8 @@
 export default function updateStudentGradeByCity(studentList, city, newGrades) {
   const updatedStudents = studentList
-    .filter(student => student.location === city)
-    .map(student => {
-      const gradeObj = newGrades.find(grade => grade.studentId === student.id);
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
       const grade = gradeObj ? gradeObj.grade : 'N/A';
       return { ...student, grade };
     });
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -3,7 +3,7 @@ export default function cleanSet(set, startString) {
     throw new Error('Invalid argument: set must be a Set');
   }
 
-  const filteredValues = [...set].filter(value => value.startsWith(startString));
-  const cleanedString = filteredValues.map(value => value.slice(startString.length)).join('-');
+  const filteredValues = [...set].filter((value) => value.startsWith(startString));
+  const cleanedString = filteredValues.map((value) => value.slice(startString.length)).join('-');
   return cleanedString;
 }
